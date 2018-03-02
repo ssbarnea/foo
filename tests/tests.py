188aaa94303615899c0a0e4d483bd47fd8be930d
Sorin Sbarnea <ssbarnea@redhat.com>
2018-03-02 21:48:23 +0000

    Added tests
    Included 3 tests and one of them randomly
    fails in ⅓ of cases.
    
    Tox execution will product test-results.xml file
    in junit format.
    
    Change-Id: I2233abdc7493594a3eef98cb6031eb3343b72557
    Signed-off-by: Sorin Sbarnea <ssbarnea@redhat.com>

---
+++ tests/tests.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from __future__ import print_function
+from random import *
+import unittest
+
+class SomeTests(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_1(self):
+        self.assertEqual(1, 1)
+
+    def test_2(self):
+        self.assertEqual(1, 1)
+
+    def test_3_randomly_fails(self):
+        self.assertNotEqual(1, randint(1, 3))
